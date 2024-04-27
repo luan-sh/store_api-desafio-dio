91ed84e262ef087839a7c9b1398c24a216cdcbd1
Luan <mydoglovestreats@gmail.com>
2024-04-27 20:39:02 -0300

    initial commit

---
+++ store/controllers/product.py
@@ -0,0 +1,41 @@
+from typing import List
+from fastapi import APIRouter, Body, Depends, HTTPException, Path, status
+from pydantic import UUID4
+
+from store.core.exceptions import NotFoundException, BaseException
+from store.schemas.product import ProductIn, ProductOut, ProductUpdate, ProductUpdateOut
+from store.usecases.product import ProductUsecase
+
+router = APIRouter(tags=["products"])
+
+@router.post(path="/", status_code=status.HTTP_201_CREATED)
+async def post(body: ProductIn = Body(...), usecase: ProductUsecase = Depends()) -> ProductOut:
+    try:
+        return await usecase.create(body=body)
+    except BaseException as exc:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=exc.message)
+
+@router.get(path="/{id}", status_code=status.HTTP_200_OK)
+async def get(id: UUID4 = Path(alias="id"), usecase: ProductUsecase = Depends()) -> ProductOut:
+    try:
+        return await usecase.get(id=id)
+    except NotFoundException as exc:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=exc.message)
+
+@router.get(path="/", status_code=status.HTTP_200_OK)
+async def query(apply_filter: bool = False, usecase: ProductUsecase = Depends()) -> List[ProductOut]:
+    return await usecase.query(apply_filter)
+
+@router.patch(path="/{id}", status_code=status.HTTP_200_OK)
+async def patch(id: UUID4 = Path(alias="id"), body: ProductUpdate = Body(...), usecase: ProductUsecase = Depends()) -> ProductUpdateOut:
+    try:
+        return await usecase.update(id=id, body=body)
+    except NotFoundException as exc:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=exc.message)
+
+@router.delete(path="/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete(id: UUID4 = Path(alias="id"), usecase: ProductUsecase = Depends()) -> None:
+    try:
+        await usecase.delete(id=id)
+    except NotFoundException as exc:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=exc.message)
